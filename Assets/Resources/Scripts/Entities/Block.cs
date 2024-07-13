be9e2e65ab433dc72af376f4f220eed68d63b373
MetaFlash <52544077+MetaFlash@users.noreply.github.com>
2024-07-13 02:54:36 -0400

    Entity fix and test

---
+++ Assets/Resources/Scripts/Entities/Block.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : Entity
+{
+    public override void Interact(Player player) {
+        Debug.Log("Yo yo yo");
+    }
+}
--- Assets/Resources/Scripts/Entities/Entity.cs
+++ Assets/Resources/Scripts/Entities/Entity.cs
@@ -4,10 +4,5 @@ using UnityEngine;
 
 public abstract class Entity : MonoBehaviour
 {
-    public bool inPast;
-    public bool inPresent;
-
-    
-
     public abstract void Interact(Player player);
 }
