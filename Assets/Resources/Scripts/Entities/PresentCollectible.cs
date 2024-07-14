6731ac7d1f94bf4f37086039b9efb1045823f497
a-fakeusername <71799509+a-fakeusername@users.noreply.github.com>
2024-07-14 10:44:41 -0500

    Sign texture, maze level
    Added new sign texture, added spotlights to the maze level, fixed padlock gate collisions

--- Assets/Resources/Scripts/Entities/PresentCollectible.cs
+++ Assets/Resources/Scripts/Entities/PresentCollectible.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PresentCollectible : Entity
 {
     public Vector2 newPosition;
+    public Sprite otherSprite;
     
     public override void Interact(Player player) {
         return;
